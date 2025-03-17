d8fe3bea0bcc649348a2f7bc11cd689cd2f6b7bf
Tataru Robert <robert.tataru98@gmail.com>
2025-03-17 18:11:22 +0200

    First commit

---
+++ Frameworks/FyberAppLovinAdapter.xcframework/ios-arm64/FyberAppLovinAdapterFramework.framework/Headers/FyberAppLovinAdapterFramework.h
@@ -0,0 +1,18 @@
+//
+//  FyberAppLovinAdapterFramework.h
+//  FyberAppLovinAdapterFramework
+//
+//  Created by Robert Tataru on 09.03.2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FyberAppLovinAdapterFramework.
+FOUNDATION_EXPORT double FyberAppLovinAdapterFrameworkVersionNumber;
+
+//! Project version string for FyberAppLovinAdapterFramework.
+FOUNDATION_EXPORT const unsigned char FyberAppLovinAdapterFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FyberAppLovinAdapterFramework/PublicHeader.h>
+
+
